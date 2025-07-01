716d61fc34f28f964f0fffe9c5c4919a1d98a9aa
guest271314 <guest271314@gmail.com>
2025-07-01 03:03:05 +0000

    Update server.mjs

--- api/server.mjs
+++ api/server.mjs
@@ -1,7 +1,7 @@
 export function GET(request) {
   return new Response("echo", {
     headers: {
-      "access-control-allow-origin": "*"
+      "access-control-allow-origin": "*",
       "cache-control": "no-cache",
       "content-type": "application/octet-stream"
     }
