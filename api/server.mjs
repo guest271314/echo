27db8689490ee4f18fb3765ba0905ca9acf60010
guest271314 <guest271314@gmail.com>
2025-07-05 16:13:02 +0000

    Update server.mjs

--- api/server.mjs
+++ api/server.mjs
@@ -1,7 +1,7 @@
 const headers = {
   "access-control-allow-origin": "*"
 };
-export function GET(request) {
+export default async function handler(request) {
   let response;
   if (request.method === "GET") {
     response = "Echo";
